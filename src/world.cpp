c8265b435b992aa003260040204c04ef4306b329
Xaver Frenz <xaverfrenz@hotmail.com>
2011-07-01 12:39:59 +0200

    + cross movent left and reight

--- src/world.cpp
+++ src/world.cpp
@@ -305,7 +305,39 @@ void World::update() {
         cross_y = 0;
     }
   }
-     
+
+ if (move_x < 0) {
+    if (cross_x > min_x) {
+      cross_x += move_x;
+      move_x = 0;
+      if (cross_x < min_x) {
+        move_x = cross_x - min_x;
+        cross_x = min_x;
+      }
+    }
+
+    if (move_x < 0 && center_x > 0) {
+      center_x += move_x;
+      move_x = 0;
+      if (center_x < 0) {
+        move_x = center_x - 0;
+        center_x = 0;
+      }
+    }
+
+    if (move_x < 0) {
+      cross_x += move_x;
+      if (cross_x < 0)
+        cross_x = 0;
+    }
+  }
+
+
+
+
+
+
+
    if (move_y > 0) {
     if (cross_y < max_y) {
       cross_y += move_y;
@@ -315,6 +347,7 @@ void World::update() {
         cross_y = max_y;
       }
     }
+
 
     if (move_y > 0 && center_y < max_center_y) {
       center_y += move_y;
@@ -329,6 +362,36 @@ void World::update() {
       cross_y += move_y;
       if (cross_y > window_height - 1)
         cross_y = window_height -1;
+    }
+  }
+  
+  
+  
+  
+   if (move_x > 0) {
+    if (cross_x < max_x) {
+      cross_x += move_x;
+      move_x = 0;
+      if (cross_x > max_x) {
+        move_x = cross_x - max_x;
+        cross_x = max_x;
+      }
+    }
+
+
+    if (move_x > 0 && center_x < max_center_x) {
+      center_x += move_x;
+      move_x = 0;
+      if (center_x > max_center_x) {
+        move_x = center_x - max_center_x;
+        center_x = max_center_x;
+      }
+    }
+
+    if (move_x > 0) {
+      cross_x += move_x;
+      if (cross_x > window_width - 1)
+        cross_x = window_width -1;
     }
   }
   
