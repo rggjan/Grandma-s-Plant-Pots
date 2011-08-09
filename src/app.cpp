14b39721ba8e89c8e0e6437a7abcfde5fa1946b5
Jan Rüegg <rggjan@gmail.com>
2011-08-09 12:25:00 +0200

    + Added better multi-window resolutions

--- src/app.cpp
+++ src/app.cpp
@@ -6,31 +6,69 @@
 #include <ClanLib/sound.h>
 
 #include <vector>
-#include <sstream>
 
 #include "./world.h"
 
-#define NUM_PLAYERS 4
+#define NUM_PLAYERS 5
 
 using std::vector;
+
+void get_display_size(int* width, int* height, int* border) {
+  // Create description
+  CL_DisplayWindowDescription desc;
+  desc.set_allow_resize(true);
+  desc.set_position(CL_Rect(100, 100, 200, 200), true);
+
+  // Calculate border
+  CL_DisplayWindow window(desc);
+  *border = window.get_geometry().top - 100;
+
+  // Maximise
+  window.maximize();
+
+  // Wait for window manager
+  while (!window.is_maximized()) {
+    CL_KeepAlive::process();
+  }
+
+  // Calculate width, height
+  *width = window.get_geometry().get_width();
+  *height = window.get_geometry().get_height();
+}
 
 int Application::main(const std::vector<CL_String> &args) {
   vector<CL_DisplayWindow*> windows;
 
-  for (int i = 0; i < NUM_PLAYERS; i++) {
+  int width;
+  int height;
+  int border;
+  get_display_size(&width, &height, &border);
+  CL_Console::write_line("w: %1, h: %2, b: %3", width, height, border);
+
+  int top_players = (NUM_PLAYERS+1)/2;
+  int window_width = width/top_players;
+  CL_Console::write_line("window_width %1", window_width);
+
+  int window_height = height/2;
+
+  for (int i = NUM_PLAYERS-1; i >= 0; i--) {
     // Create a window description
     CL_DisplayWindowDescription desc;
-    std::ostringstream stream;
-    stream << "Grandma's Plant Pots: Player " << i;
-    desc.set_title(stream.str());
+    desc.set_title(cl_format("GPP: Player %1", i));
 
-    // Use this resolution (as caption is disabled)
-    //desc.set_size(CL_Size(500, 500), true);
-    int window_width = 400;
-    int window_height = 400;
-    desc.set_position(CL_Rect(window_width*(i%2), window_height*(i/2),
-                              window_width*(i%2)+window_width,
-                              window_height*(i/2)+window_height), false);
+    if (NUM_PLAYERS == 2) {
+      desc.set_position(CL_Rect(i*width/2, 0, (i+1)*width/2, height), false);
+    } else {
+      if (i < top_players) {
+        desc.set_position(CL_Rect(i*window_width, 0,
+                                  (i+1)*window_width, window_height),
+                          false);
+      } else {
+        desc.set_position(CL_Rect((i-top_players)*window_width, border+window_height,
+                                  (i-top_players+1)*window_width, border+2*window_height),
+                          false);
+      }
+    }
 
     CL_DisplayWindow* window = new CL_DisplayWindow(desc);
     windows.push_back(window);
