9c38db2b2965507b86398e90f74e577d22742b97
Jan Rüegg <rggjan@gmail.com>
2011-08-26 20:27:57 +0200

    ! Added define for window size calculations

--- src/app.cpp
+++ src/app.cpp
@@ -10,6 +10,7 @@
 #include "./world.h"
 
 #define NUM_PLAYERS 4
+#define CALCULATE_WINDOW_SIZES false
 
 using std::vector;
 
@@ -38,49 +39,69 @@ void get_display_size(int* width, int* height, int* border) {
 
 int Application::main(const std::vector<CL_String> &args) {
   vector<CL_DisplayWindow*> windows;
-  windows.resize(NUM_PLAYERS);
 
-  int width;
-  int height;
-  int border;
-  get_display_size(&width, &height, &border);
-  CL_Console::write_line("w: %1, h: %2, b: %3", width, height, border);
+  if (CALCULATE_WINDOW_SIZES) {
+    windows.resize(NUM_PLAYERS);
 
-  int top_players = (NUM_PLAYERS+1)/2;
-  int window_width = width/top_players;
-  CL_Console::write_line("window_width %1", window_width);
+    int width;
+    int height;
+    int border;
+    get_display_size(&width, &height, &border);
+    CL_Console::write_line("w: %1, h: %2, b: %3", width, height, border);
 
-  int window_height = height/2;
+    int top_players = (NUM_PLAYERS+1)/2;
+    int window_width = width/top_players;
+    CL_Console::write_line("window_width %1", window_width);
 
-  for (int i = NUM_PLAYERS-1; i >= 0; i--) {
-    // Create a window description
-    CL_DisplayWindowDescription desc;
-    desc.set_title(cl_format("GPP: Player %1", i));
+    int window_height = height/2;
 
-    CL_Rect position;
-    if (NUM_PLAYERS == 2) {
-      position = CL_Rect(i*width/2, 0, (i+1)*width/2, height);
-    } else {
-      if (i < top_players) {
-        position = CL_Rect(i*window_width, 0,
-                                  (i+1)*window_width, window_height);
+    for (int i = NUM_PLAYERS-1; i >= 0; i--) {
+      // Create a window description
+      CL_DisplayWindowDescription desc;
+      desc.set_title(cl_format("GPP: Player %1", i));
+
+      CL_Rect position;
+      if (NUM_PLAYERS == 2) {
+        position = CL_Rect(i*width/2, 0, (i+1)*width/2, height);
       } else {
-        position = CL_Rect((i-top_players)*window_width,
-                                  border+window_height,
-                                  (i-top_players+1)*window_width,
-                                  border+2*window_height);
+        if (i < top_players) {
+          position = CL_Rect(i*window_width, 0,
+                             (i+1)*window_width, window_height);
+        } else {
+          position = CL_Rect((i-top_players)*window_width,
+                             border+window_height,
+                             (i-top_players+1)*window_width,
+                             border+2*window_height);
+        }
       }
-    }
-    desc.set_position(position, false);
+      desc.set_position(position, false);
 
-    CL_DisplayWindow* window = new CL_DisplayWindow(desc);
+      CL_DisplayWindow* window = new CL_DisplayWindow(desc);
 
-    // TODO(rggjan): HACK to make it work...
-    while (std::abs(desc.get_position().left-window->get_geometry().left) > 1) {
-      window->set_position(position, false);
-      CL_KeepAlive::process();
+      // TODO(rggjan): HACK to make it work...
+      while (std::abs(desc.get_position().left-window->get_geometry().left) > 1) {
+        window->set_position(position, false);
+        CL_KeepAlive::process();
+      }
+      windows[i] = window;
     }
-    windows[i] = window;
+  } else {
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+      // Create a window description
+      CL_DisplayWindowDescription desc;
+      desc.set_title(cl_format("GPP: Player %1", i));
+
+      // Use this resolution (as caption is disabled)
+      //desc.set_size(CL_Size(500, 500), true);
+      int window_width = 400;
+      int window_height = 400;
+      desc.set_position(CL_Rect(window_width*(i%2), window_height*(i/2),
+                                window_width*(i%2)+window_width,
+                                window_height*(i/2)+window_height), false);
+
+      CL_DisplayWindow* window = new CL_DisplayWindow(desc);
+      windows.push_back(window);
+    }
   }
 
   CL_SoundOutput output(44100);
